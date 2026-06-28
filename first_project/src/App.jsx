4d29fdbc52b3ee9aa6c6c053485e61487a2bfe1e
Poorvi551 <182614650+Poorvi551@users.noreply.github.com>
2026-06-28 19:43:05 +0530

    add first_project folder

---
+++ first_project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./components/Navbar.jsx";
+const App = () => {
+  console.log("Good morning!");
+  return(
+    <>
+    <Navbar/>
+    <Navbar/>
+    <Navbar/>
+    </>
+  )
+}
+export default App;
